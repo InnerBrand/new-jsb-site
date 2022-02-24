96f4988f4b11f628b0dc5b7f389c0b553adaddb2
InnerBrand <joshua@innerbrandconcierge.com>
2022-02-24 14:47:52 -0600

    Team page sections going, need Members sections next

--- client/src/components/Quote.js
+++ client/src/components/Quote.js
@@ -2,33 +2,29 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 // Styles
-import * as styles from '../styles/modules/Quote.module.scss'
+import * as styles from '../styles/modules/Quote.module.scss';
 
 // Images
-import TestPic from '../images/testimonial-person-1.png'
+import TestPic from '../images/quote-1.png';
 
 const Quote = () => {
   return (
     <section className={styles.wrapper}>
-        <div className={styles.content}>
-          <div className={styles.image}>
-            <img src={TestPic} />
-          </div>
-          <div className={styles.info}>
-            <h3>
-              JSB team has guided me over the last 5 years, helping me find two of my previous roles.
-            </h3>
-            <h3>
-              John Stanson | CFO at Yahia
-            </h3>
-            <Link to="/"> Read more success stories</Link>
-
-          </div>
+      <div className={styles.content}>
+        <div className={styles.image}>
+          <img src={TestPic} />
         </div>
-    
+        <div className={styles.info}>
+          <h4>
+            "Joe goes above and beyond helping our company find the best talent
+            out there"
+          </h4>
+          <h5>John, hiring executive & repeat client</h5>
+          {/* <Link to='/'> Read more success stories</Link> */}
+        </div>
+      </div>
     </section>
-  
-  )
+  );
 };
 
 export default Quote;
--- client/src/pages/team.js
+++ client/src/pages/team.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 
 // Components
 import Quote from '../components/Quote';
 
-const TeamPage = () => {
-  return <div>
-    <Quote />
-  </div>;
+// Styles
+import * as styles from '../styles/modules/pages/Team.module.scss';
+import Layout from '../components/Layout';
+import Blob from '../components/Blob';
+
+const TeamPage = ({ data }) => {
+  const headline = data.allSanityTeamPage.nodes[0].heroHeadline;
+  return (
+    <>
+      <Layout>
+        <h1 className={styles.heroHeadline}>{headline}</h1>
+        <Quote />
+      </Layout>
+    </>
+  );
 };
+
+export const query = graphql`
+  query MyQuery {
+    allSanityTeamPage {
+      nodes {
+        heroHeadline
+      }
+    }
+  }
+`;
 
 export default TeamPage;
