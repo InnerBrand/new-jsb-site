5c78b15eecc3304b342192cad058b2ce78ddafca
InnerBrand <joshua@innerbrandconcierge.com>
2022-06-17 14:01:41 -0500

    updated LadyQuote on /team page

--- client/src/components/LadyQuote.js
+++ client/src/components/LadyQuote.js
@@ -16,10 +16,13 @@ const LadyQuote = () => {
         </div>
         <div className={styles.info}>
           <h4>
-            "Their entire team has shown genuine care for every step of my
-            career journey."
+            "I’ve had the pleasure of working with Eika Ng over the last 10
+            years. She’s helped me progress throughout my career, always looking
+            out for my best interest and is always sincere, straightforward,
+            professional and thoughtful. She genuinely cares for your success
+            and in finding the right fit and role for you. Thank you Eika!"
           </h4>
-          <h5>Sarah, Financial Analyst</h5>
+          <h5>- Jhoanna</h5>
           {/* <Link to='/'> Read more success stories</Link> */}
         </div>
       </Container>
