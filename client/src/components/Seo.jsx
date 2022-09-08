b1c0bdfc3c48456be74cf99e7e6d19be7852cb3a
Fernando <lebol12051@otodir.com>
2022-09-08 12:02:15 -0500

    delete comment

--- client/src/components/Seo.jsx
+++ client/src/components/Seo.jsx
@@ -23,15 +23,6 @@ export const SEO = ({ title, description, pathname, children }) => {
       <title>{seo.title}</title>
       <meta name='description' content={seo.description} />
       <meta name='image' content={seo.image} />
-      {/* <link
-        rel='icon'
-        type='image/x-icon'
-        href='../assets/images/icon.png'
-      ></link> */}
-      {/* <link
-        rel='icon'
-        href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>👤</text></svg>"
-      /> */}
       {children}
     </>
   );
